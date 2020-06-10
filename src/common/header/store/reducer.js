a968294568a1a753b2370c266e6995e7ed8dbd41
Unusualmay <2409029740@qq.com>
2020-06-11 00:03:02 +0800

    使用immutable.js来使得store中的state数据不可发生改变

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,20 +1,19 @@
 // reducer返回的是一个纯函数，也就是有固定输入，就会有固定输出的
 import {constant} from "./index";
+import {fromJS} from 'immutable';
 
-const defaultState = {
+const defaultState = fromJS({
     focused: false
-};
+});
 
 export default (state = defaultState, action) => {
     if (action.type === constant.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        //immutable对象的set方法，会结合之前的immutable对象的值
+        //和设置的值，返回一个全新的对象
+        return state.set('focused', true)
     }
     if (action.type === constant.SEARCH_BLUR) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false)
     }
     return state;
 }
